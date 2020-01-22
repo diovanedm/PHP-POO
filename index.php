2133c800f34b25aec7e068fc0634bca95281f92a
diovanedm <diovane.dm@gmail.com>
2020-01-22 10:30:20 -0300

    Clonagem

--- index.php
+++ index.php
@@ -8,10 +8,12 @@ class Pessoa {
 $pessoa = new Pessoa();
 $pessoa->idade = 25;
 
-$pessoa2 = $pessoa;
+$pessoa2 = clone $pessoa;
 $pessoa2->idade = 35;
 
 echo $pessoa->idade;
+echo $pessoa2->idade;
+
 
 
 
