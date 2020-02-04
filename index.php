6ee13275ba827211d442db1e0a458d7e5a36a987
diovanedm <diovane.dm@gmail.com>
2020-02-04 14:31:17 -0300

    Composer e bibliotecas

--- index.php
+++ index.php
@@ -1,51 +1,10 @@
 <?php 
 
-/* Informa o nível dos erros que serão exibidos */
-error_reporting(E_ALL);
- 
-/* Habilita a exibição de erros */
-ini_set("display_errors", 1);
+require_once 'vendor/autoload.php';
 
+use Cocur\Slugify\Slugify;
 
-# Métodos mágicos
-
-class Pessoa {
-    private $dados;
-
-    public function __set($dado, $valor){
-        $this->dados[$dado] = $valor;
-    }
-
-    public function __get($dado){
-        return $this->dados[$dado];
-    }
-
-    public function __tostring() {
-        return "Tentei imprimir o objeto";
-    }
-
-    public function __invoke() {
-        return "Objeto como função";
-    }
-}
-
-$pessoa = new Pessoa();
-$pessoa->nome = "Diovane";
-$pessoa->dado = 21;
-$pessoa->sexo = "Masculino";
-
-echo $pessoa->nome;
-echo $pessoa->dado;
-echo $pessoa->sexo; 
-
-echo "<br>";
-
-echo $pessoa;
-
-echo "<br>";
-
-echo $pessoa();
-
-
-
-
+$slugify = new Slugify();
+$slugify->addRule("Hello", "oi");
+$slugify->addRule("World", "Mundo");
+echo $slugify->slugify('Hello World!', '_');
